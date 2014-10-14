a78d4edd02b6f662185100123c3d805009aa38f7
Arek Krawczyk <ares_real@wp.pl>
2014-10-14 19:42:02 +0200

    Further code improvement, duck-typing instead of explicit inheritance

--- article.rb
+++ article.rb
@@ -1,48 +1,48 @@
-class Article
+class  Article
 
- attr_reader :title,:content,:author,:likes,:dislikes,:created_at
+  attr_reader  :title,:content,:author,:likes,:dislikes,:created_at
 
- def initialize(title, content, author = "")
-    @title, @content, @author = title, content, author
-    @likes = @dislikes = 0
- end
-
- def like!
-  @likes += 1
- end
- 
- def dislike!
-  @dislikes += 1
- end
-
- def positive_votes
-  @likes - @dislikes
- end
-
- def votes
-  @likes + @dislikes
- end
-
- def shortened_to(limit)
-  if limit - 3 < 1
-   raise "Error! Negative argument"
+  def initialize(title, content, author = "")
+      @title, @content, @author = title, content, author
+      @likes = @dislikes = 0
   end
-  if @content.length < limit-3
-   return @content
+
+  def like!
+    @likes += 1
   end
-  @content[0,limit-4] + "..."
- end
+  
+  def  dislike!
+    @dislikes += 1
+  end
 
- def include?(string)
-  @content.include?(string)
- end
+  def  positive_votes
+    @likes - @dislikes
+  end
 
- def words
-  content.downcase.gsub(/[^a-z ]/, " ").split(' ')
- end
+  def  votes
+    @likes + @dislikes
+  end
 
- def unique_words
-  words.uniq
- end
+  def shortened_to(limit)
+    if limit - 3 < 1
+      raise "Error! Negative argument"
+    end
+    if @content.length < limit-3
+      return @content
+    end
+    @content[0,limit-4] + "..."
+  end
+
+  def include?(string)
+    @content.include?(string)
+  end
+
+  def words
+    content.downcase.gsub(/[^a-z ]/, " ").split(' ')
+  end
+
+  def unique_words
+    words.uniq
+  end
 
 end
--- article_manager.rb
+++ article_manager.rb
@@ -1,73 +1,71 @@
-require './Article.rb'
+require  './article.rb'
 
-class ArticleManager
- @arrticles
- def initialize()
-  @arrticles = []
- end
- 
- def add(article)
-  if article.instance_of? Article or article.class.ancestors.include? Article
-   @arrticles += [article]
-   return self
+class  ArticleManager
+
+  def initialize()
+    @arrticles = []
   end
-  raise "Not an article!"
- end
-
- def delete(article)
-  if article.instance_of? Article or article.class.ancestors.include? Article
-   @arrticles += [article]
-   return self
+  
+  def add(article)
+    if !article.respond_to?(:positive_votes) && !article.respond_to?(:votes)  \
+          && !article.respond_to?(:shortened_to) && !article.respond_to?(:include?)
+      raise "Not  an  article!"
+    end
+    @arrticles += [article]
   end
-  raise "Not an article!"
- end
 
- def worst_articles
-  if @arrticles.length == 0
-   raise "No articles in the manager!"
+  def delete(article)
+    if article.respond_to?(:positive_votes) && article.respond_to?(:votes)  \
+          && article.respond_to?(:shortened_to) && article.respond_to?(:include?)
+      raise "Not an article!"
+    end
+    @arrticles.delete(article)
+
   end
-  @arrticles.sort_by { |x| x.positive_votes }
- 
- end
 
- def best_articles()
-  if @arrticles.length == 0
-   raise "No articles in the manager!"
+  def worst_articles
+    if @arrticles.length == 0
+      raise "No articles in the manager!"
+    end
+    @arrticles.sort_by { |x| x.positive_votes }
+  
   end
-  @arrticles.sort_by {|x| x.positive_votes*-1 }
- end
 
- def best_article
-  best_articles()[0]
- end
-
- def worst_article
-  worst_articles()[0]
- end
-
- def votes
-  if @arrticles.length == 0
-   raise "No articles in the manager!"
+  def best_articles()
+    if @arrticles.length == 0
+      raise "No articles in the manager!"
+    end
+    @arrticles.sort_by {|x| x.positive_votes*-1 }
   end
-  sumOfVotes = 0
-  arrticles.each { |x| sumOfVotes += x.votes }
-  sumOfVotes
- end
 
- def to_s
-  list_of_articles = ""
-  i=1
-  @arrticles.each do|x| 
-   list_of_articles += "- #{x.title} #{x.shortened_to(10)}"
+  def best_article
+    best_articles()[0]
   end
-  list_of_articles
- end
 
- def include?(pattern)
-  if @arrticles.length == 0
-   raise "No articles in the manager!"
+  def worst_article
+    worst_articles()[0]
   end
-  @arrticles.delete_if {|x| !x.include?(pattern)} 
- end
+
+  def votes
+    if @arrticles.length == 0
+      raise "No articles in the manager!"
+    end
+    sumOfVotes = 0
+    arrticles.each { |x| sumOfVotes += x.votes }
+    sumOfVotes
+  end
+
+  def to_s
+    list_of_articles = ""
+    @arrticles.each { |x| list_of_articles += "-  #{x.title} #{x.shortened_to(10)}" }
+    list_of_articles
+  end
+
+  def include?(pattern)
+    if @arrticles.length == 0
+      raise "No  articles in the manager!"
+    end
+    @arrticles.delete_if {|x| !x.include?(pattern)} 
+  end
 
 end
