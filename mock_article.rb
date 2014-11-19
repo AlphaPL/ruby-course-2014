c82df8f7c104c3d048cbcd1121cc00c0560c2d19
AlphaPL <ares_real@wp.pl>
2014-11-19 18:34:29 +0100

    [#17] Making use of newly created gem.

--- mock_article.rb
+++ mock_article.rb
@@ -1,39 +1,11 @@
 require_relative  './article.rb'
 require_relative  './article_manager.rb'
-
+require 'blabla'
 class MockArticle < Article
-
-  WORD_LIST = File.read(File.dirname(__FILE__)+"/randomWords.txt").split
-
   def initialize
-    super(generate_title,generate_article,generate_author)
+    super(Blabla.generate_title,Blabla.generate_article,Blabla.generate_author)
     @likes = random_number
     @likes = random_number
-  end
-
-  private
-
-  def get_random_words(n)
-    WORD_LIST.sample(n)
-  end
-
-  def generate_author
-    author = get_random_words(2)
-    "#{author[0].capitalize} #{author[1].capitalize}"
-  end
-
-  def generate_sentence
-    get_random_words(rand(9)+1).join(' ').capitalize << "."
-  end
-
-  def generate_title
-    get_random_words(rand(4)+1).join(' ').capitalize << "."
-  end
-
-  def generate_article
-    article = ""
-    rand(10).times { article += generate_sentence }
-    article
   end
 
   def random_number
